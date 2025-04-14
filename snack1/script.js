cf5703f180f65cec8ccb3b1d15c490b0fa221b77
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-04-14 15:49:24 +0200

    end

--- snack1/script.js
+++ snack1/script.js
@@ -1,40 +1,34 @@
 // creo array di bici
 
-const bicycles = [];
-
-const bikeone = {
-  name: "one",
-  peso: 25,
-};
-const biketwo = {
-  name: "two",
-  peso: 35,
-};
-const bikethree = {
-  name: "three",
-  peso: 52,
-};
-const bikefour = {
-  name: "four",
-  peso: 43,
-};
-const bikefive = {
-  name: "five",
-  peso: 38,
-};
-
-bicycles.push(bikeone);
-bicycles.push(biketwo);
-bicycles.push(bikethree);
-bicycles.push(bikefour);
-bicycles.push(bikefive);
+const bicycles = [
+  {
+    name: "bikeone",
+    peso: 25,
+  },
+  {
+    name: "biketwo",
+    peso: 35,
+  },
+  {
+    name: "bikethree",
+    peso: 52,
+  },
+  {
+    name: "bikefour",
+    peso: 43,
+  },
+  {
+    name: "bikefive",
+    peso: 38,
+  },
+];
 
 console.log(bicycles);
 
 // stampiamo le bici con il peso minore
 
 for (const lowweightbyke of bicycles) {
-  if (bicycles[1].peso <= 40) {
+  if (lowweightbyke.peso < 40) {
     console.log(lowweightbyke);
   }
 }
